2a57f08e4252461abfa0039a6afaa9fa65c323d3
leriahes <valeriya-shevchenko-2002@mail.ru>
2023-09-20 20:32:17 +0700

    Base

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 import pygame as pg
+from object3D import *
+from camera import *
+from projection import *
 
 class SoftwareRender:
     def __init__(self):
@@ -8,13 +11,30 @@ class SoftwareRender:
         self.FPS = 60
         self.screen = pg.display.set_mode(self.RES)
         self.clock = pg.time.Clock()
+        self.create_objects()
+
+    def create_objects(self):
+        self.camera = Camera(self, [0.5, 1, -4])
+        self.projection = Projection(self)
+        self.object = Object3D(self)
+        self.object.translate([0.2, 0.4, 0.2])
+        self.axes = Axes(self)
+        self.axes.translate([0.7, 0.9, 0.7])
+        self.world_axes = Axes(self)
+        self.world_axes.movement_flag = False
+        self.world_axes.scale(2.5)
+        self.world_axes.translate([0.0001, 0.0001, 0.0001])
 
     def draw(self):
         self.screen.fill(pg.Color('dimgrey'))
+        self.world_axes.draw()
+        self.axes.draw()
+        self.object.draw()
 
     def run(self):
         while True:
             self.draw()
+            self.camera.control()
             [exit() for i in pg.event.get() if i.type == pg.QUIT]
             pg.display.set_caption(str(self.clock.get_fps()))
             pg.display.flip()
